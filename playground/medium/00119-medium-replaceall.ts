c492eadc57df5c1a481c68061c66e18ac7d4d08d
LovePlayCode <1244224501@qq.com>
2026-01-05 10:16:51 +0800

    feat: 实现 ReplaceAll 类型的逻辑

--- playground/medium/00119-medium-replaceall.ts
+++ playground/medium/00119-medium-replaceall.ts
@@ -18,7 +18,11 @@
 
 /* _____________ 你的代码 _____________ */
 
-type ReplaceAll<S extends string, From extends string, To extends string> = any
+type ReplaceAll<S extends string, From extends string, To extends string> = From extends ''
+  ? S
+  : S extends `${infer Before}${From}${infer After}`
+    ? `${Before}${To}${ReplaceAll<After, From, To>}`
+    : S
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
